74ffd6c3a52a4c6f48daee1c2b01045970ba74c7
Jordan Olsen <jolsen@neslotech.co.za>
2019-10-02 17:59:25 +0200

    Created user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
@@ -65,5 +64,4 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-
 end
