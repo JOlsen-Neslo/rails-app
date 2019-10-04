43a2d9bb160d9e8e4e104aec58be92d342029f15
Jordan Olsen <jordan@silvermoongroup.com>
2019-10-04 12:34:53 +0200

    Modifications for style guide

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -23,5 +22,4 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
-
 end
