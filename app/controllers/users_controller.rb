43c7a306414eef959288bbce66ce81dc1802c9ea
Jordan Olsen <46996097+JOlsen-Neslo@users.noreply.github.com>
2019-10-08 11:56:15 +0200

    Chapter 7: Sign up a user (#5)
    * Finish user signup.
    
    * Modifications for style guide
    
    * Code review changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params.require(:id))
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+      flash[:success] = I18n.t 'users.new.create.success'
+      redirect_to @user
+    else
+      flash[:error] = I18n.t 'users.new.create.error'
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params
+        .require(:user)
+        .permit(
+            :name,
+            :email,
+            :password,
+            :password_confirmation
+        )
   end
 end
